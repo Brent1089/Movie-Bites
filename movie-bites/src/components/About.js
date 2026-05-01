a22481b3f715d5ce48ceb876e2c2caa7456249fd
Brent Deutscher <36459485+Brent1089@users.noreply.github.com>
2026-04-30 23:32:57 -0500

    Update About.js

--- movie-bites/src/components/About.js
+++ movie-bites/src/components/About.js
@@ -12,9 +12,9 @@ export default function About() {
                     <ul>
                         <li><strong>Name: </strong> Brent Deutscher</li>
                         <li><strong>Occupation: </strong> Student </li>
-                        <li><strong>Email: </strong><a href="mailto:deutschb4@mail.gtc.edu">deutschb4@mail.gtc.edu</a></li>
+                        <li><strong>Email: </strong><a href="mailto:brent1089@gmail.com">brent1089@gmail.com</a></li>
                         <li><strong>Portfolio: </strong><a href="https://prometheus.gtc.edu/~bdeutscher/">https://prometheus.gtc.edu/~bdeutscher/</a></li>
-                        <li>I am a second year student at Gateway Technical College.</li>
+                        <li>I am a web software developer graduate from Gateway Technical College.</li>
                     </ul>
                 </div>
             </div>
